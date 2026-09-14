33980cf1de820bd2e9173d90edeca0ae8b221dab
Manas Gandhi <manas.25bcs10617@sst.scaler.com>
2026-09-15 00:27:34 +0530

    Remove unused commands listener

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,4 @@
 // Allows opening side panel on action click
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(console.error);
 
-// Optional: Keyboard shortcut listener could be implemented here using chrome.commands
-chrome.commands.onCommand.addListener((command) => {
-    if (command === "_execute_action") {
-        // Triggering side panel opening via command is native if openPanelOnActionClick is set
-    }
-});
+
